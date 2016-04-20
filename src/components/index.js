11ba7b60d17c8a96fb0251ccc9b8b6ff8e3cd513
francis <cloud9+cifren8x@gmail.com>
2016-04-20 08:58:09 +0000

    add componenent for export - transform index into ES6 file

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import _EditPage from "./EditPage";
+export {_EditPage as EditPage}
+import _CreatePage from "./CreatePage";
+export {_CreatePage as CreatePage}
+import _ListPage from "./ListPage";
+export {_ListPage as ListPage}
+import _ShowPage from "./ShowPage";
+export {_ShowPage as ShowPage}
+
+const components = {
+  "EditPage": _EditPage,
+  "CreatePage": _CreatePage,
+  "ListPage": _ListPage,
+  "ShowPage": _ShowPage
+};
+
+export default components;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
-//pool / admin config / rest
+// Components
 "use strict";
 
-exports.__esModule = true;
+import {pool as _PoolReducer} from "./reducers/adminReducer";
+export {_PoolReducer as poolReducer};
 
-var _PoolReducer = require("./reducers/adminReducer");
-var _PoolReducer2 = _interopRequireDefault(_PoolReducer.pool);
+import _AdminConfig from "./models/AdminConfig";
+export { _AdminConfig as AdminConfig };
 
-var _AdminConfig = require("./models/AdminConfig");
-var _AdminConfig2 = _interopRequireDefault(_AdminConfig);
+import _AdminPage from "./containers/AdminPage";
+export { _AdminPage as AdminPage };
 
-var _AdminPage = require("./containers/AdminPage");
-var _AdminPage2 = _interopRequireDefault(_AdminPage);
+import _Rest from "./rests/rest";
+export { _Rest as rest };
 
-var _Rest = require("./rests/rest");
-var _Rest2 = _interopRequireDefault(_Rest);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-exports.poolReducer = _PoolReducer2["default"];
-exports.rest = _Rest2["default"];
-exports.AdminConfig = _AdminConfig2["default"];
-exports.AdminPage = _AdminPage2["default"];
+import _components, {EditPage as _EditPage, CreatePage as _CreatePage, ListPage as _ListPage, ShowPage as _ShowPage} from "./components/index";
+export { _components as components };
+export { _EditPage as EditPage };
+export { _CreatePage as CreatePage };
+export { _ListPage as ListPage };
+export { _ShowPage as ShowPage };
