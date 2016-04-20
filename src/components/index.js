57f8f6153013da50f0290497a769e609e2572677
francis <cloud9+cifren8x@gmail.com>
2016-04-20 09:17:47 +0000

    fix lint semicolon missing

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
 import _EditPage from "./EditPage";
-export {_EditPage as EditPage}
+export {_EditPage as EditPage};
 import _CreatePage from "./CreatePage";
-export {_CreatePage as CreatePage}
+export {_CreatePage as CreatePage};
 import _ListPage from "./ListPage";
-export {_ListPage as ListPage}
+export {_ListPage as ListPage};
 import _ShowPage from "./ShowPage";
-export {_ShowPage as ShowPage}
+export {_ShowPage as ShowPage};
 
 const components = {
   "EditPage": _EditPage,
